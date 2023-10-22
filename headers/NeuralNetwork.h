9444d266cd3ddf93906875ad8c2f53acccb9d575
Mateusz Zynek <mateuszzynek@gmail.com>
2023-10-22 14:34:27 +0200

    Feed forward for neural network and debugging function showing the neuron outputs

--- headers/NeuralNetwork.h
+++ headers/NeuralNetwork.h
@@ -1,11 +1,16 @@
 #pragma once
 #include "Neuron.h"
 #include <vector>
+#include <iostream>
 
 class NeuralNetwork
 {
 private:
 	std::vector<std::vector<Neuron>> layers;
 public:
-	NeuralNetwork(const std::vector<unsigned> topology);
+	NeuralNetwork(const std::vector<unsigned>& topology);
+	void feedForfward(const std::vector<double>& inputValues);
+	void backPropagation(const std::vector<double>& targetValues);
+	void getResults(std::vector<double>& resultVals) const;
+	void printNeuronOutputs();
 };
--- sources/NeuralNetwork.cpp
+++ sources/NeuralNetwork.cpp
@@ -1,7 +1,7 @@
 #include "../headers/NeuralNetwork.h"
 
 
-NeuralNetwork::NeuralNetwork(const std::vector<unsigned> topology)
+NeuralNetwork::NeuralNetwork(const std::vector<unsigned>& topology)
 {
 	for (int layerNum = 0; layerNum < topology.size(); layerNum++)
 	{
@@ -17,6 +17,42 @@ NeuralNetwork::NeuralNetwork(const std::vector<unsigned> topology)
 		for (int neuronNum = 0; neuronNum <= topology[layerNum]; neuronNum++)
 		{
 			layers.back().push_back(Neuron(numOfOutputs, neuronNum));
+		}
+	}
+}
+
+void NeuralNetwork::feedForfward(const std::vector<double>& inputValues)
+{
+	// check if provided number of values is the same as number of inputs in NeuralNet
+	if (inputValues.size() != layers[0].size() - 1)
+	{
+		std::cout << "Incorrect number of input values" << std::endl;
+		exit(1);
+	}
+
+	// Assign input values -> input neurons
+	for (int i = 0; i < inputValues.size(); i++)
+		layers[0][i].setOutputValue(inputValues[i]);
+
+	for (int layerNum = 1; layerNum < layers.size(); layerNum++)
+	{
+		std::vector<Neuron>& prevLayer = layers[layerNum - 1];
+		for (int i = 0; i < layers[layerNum].size() - 1; i++)
+		{
+			layers[layerNum][i].feedForward(prevLayer);
+		}
+	}
+}
+
+void NeuralNetwork::printNeuronOutputs()
+{
+	for (int i = 0; i < layers.size(); i++)
+	{
+		for (int j = 0; j < layers[i].size(); j++)
+		{
+			std::cout << "Layer: " << i << std::endl;
+			std::cout << "Neuron: " << j << std::endl;
+			std::cout << "Value: " << layers[i][j].getOutputValue() << "\n\n";
 		}
 	}
 }
